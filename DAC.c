34b53d6f572a1460addd10835c4717e259a8cd34
logancrow <43042565+logancrow@users.noreply.github.com>
2018-10-03 21:28:30 -0500

    Add files via upload

---
+++ DAC.c
@@ -0,0 +1,21 @@
+//DAC.c
+//function definitions for interfacing 12 bit DAC
+//Logan Crow and Samantha Flaim
+//Date Created: 10-3-18
+//Date Last Modified: 10-3-18
+
+#include "DAC.h"
+
+//initializes SSI1 for DAC interface
+//input parameters: none
+//output parameters: none
+void DAC_Init(void){
+	
+}
+
+//outputs value to DAC
+//input parameters: value to send to DAC
+//output parameters: none
+void DAC_Out(uint16_t value){
+	
+}
---
+++ Switch.h
@@ -0,0 +1,17 @@
+//Switch.h
+//function decleration for Switch interfacing for 12 bit DAC
+//Logan Crow and Samantha Flaim
+//Date Created: 10-3-18
+//Date Last Modified: 10-3-18
+
+
+
+//Initializes PB0 and PB1 to be inputs
+//input parameters: none
+//output parameters: none
+void SwitchInit(void);
+
+//Plays/Pauses music by enabling/disabling Timer0
+//input parameters: none
+//output parameters: none
+void Pause(void);
