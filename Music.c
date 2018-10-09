d15fab838571e823de73b4f052042a5f58075bf5
samanthaflaim <samanthaflaim@utexas.edu>
2018-10-08 19:41:04 -0500

    song

--- Music.c
+++ Music.c
@@ -5,3 +5,17 @@
 //Date Last Modified: 10-3-18
 
 #include "Music.h"
+
+struct note n1;
+struct note n2;
+struct note n3;
+struct note n4;
+
+struct note song[100] = {
+	 {d2, qnote}, {g1, qnote}, {d2, hnote}, {d2, qnote}, {g1, qnote},
+	 {d2, qnote}, {g1, qnote}, {d2, hnote}, {d2, qnote}, {g1, qnote},
+	 {e2, qnote}, {a1, qnote}, {e2, hnote}, {e2, qnote}, {b2, qnote},
+	 {e2, qnote}, {e2, qnote}, {c2, qnote}, {b2, qnote}, {a1, qnote}, {b2, qnote},
+	 {g1, qnote}, {g1, qnote}, {a2, hnote}, {a2, hnote},
+	 {a2, hnote}, {gf2, qnote}, {a2, qnote}, {gf2, qnote}, {d2, qnote}
+};
--- Music.h
+++ Music.h
@@ -6,8 +6,29 @@
 
 #include "stdint.h"
 
+const double e1 = 659.26;
+const double f1 = 698.46;
+const double gf1 = 739.99;
+const double g1 = 783.99;
+const double a1 = 880;
+const double b1 = 987.77;
+const double c1 = 1046.5;
+const double d2 = 1174.66;
+const double e2 = 1318.51;
+const double f2 = 1396.91;
+const double gf2 = 1479.98;
+const double g2 = 1567.98;
+const double a2 = 1760;
+const double b2 = 1975.53;
+const double c2 = 2093;
+
+const uint8_t enote = 1;
+const uint8_t qnote = 2;
+const uint8_t hnote = 4;
+const uint8_t wnote = 8;
+
 struct note{
-	uint32_t frequency;
+	double freq;
 	uint32_t length;
 };
 
